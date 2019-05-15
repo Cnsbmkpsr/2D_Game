f2e5d6fce52aaf6861fa152d4e7ae04bec1a4503
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2019-05-16 00:37:14 +0200

    Commit 16/05/2019 00:36 add KeyInput.Java

---
+++ 2D_game/src/data/main/KeyInput.java
@@ -0,0 +1,27 @@
+package data.main;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyInput extends KeyAdapter {
+
+	
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
